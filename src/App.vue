a0352e547eaa567376d1001c391e70d17537cd5e
u7663394 <u7663394@anu.edu.au>
2026-03-14 22:51:53 +1100

    Add top-level route configuration

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,6 @@
 <template>
   <div id="app">
     <router-view> </router-view>
-    <!-- Test postcss -->
-    <div class="box"></div>
   </div>
 </template>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,19 @@
+import Detail from "@/views/Detail.vue";
+import Layout from "@/views/Layout.vue";
+import Login from "@/views/Login.vue";
+import Register from "@/views/Register.vue";
 import Vue from "vue";
 import VueRouter from "vue-router";
 
 Vue.use(VueRouter);
 
 const router = new VueRouter({
-  routes: [],
+  routes: [
+    { path: "/login", component: Login },
+    { path: "/register", component: Register },
+    { path: "/layout", component: Layout },
+    { path: "/article/:id", component: Detail },
+  ],
 });
 
 export default router;
