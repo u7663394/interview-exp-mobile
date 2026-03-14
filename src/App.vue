f2c0df762ce1717d10c0b9c41c1fad11d9795c9f
u7663394 <u7663394@anu.edu.au>
2026-03-14 22:23:19 +1100

    Config Vant

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
     <router-view> </router-view>
+    <!-- Test Vant Config -->
+    <van-button type="primary">按钮</van-button>
   </div>
 </template>
 
---
+++ src/utils/vant-ui.js
@@ -0,0 +1,4 @@
+// 按需导入 Vant
+import Vue from "vue";
+import { Button } from "vant";
+Vue.use(Button);
