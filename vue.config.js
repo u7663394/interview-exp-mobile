eef450f1b5b5faf325d7481286282807fc32b96c
u7663394 <u7663394@anu.edu.au>
2026-03-15 13:40:41 +1100

    Configure theme color

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,15 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  css: {
+    loaderOptions: {
+      less: {
+        lessOptions: {
+          modifyVars: {
+            blue: "#FA6D1D",
+          },
+        },
+      },
+    },
+  },
 });
