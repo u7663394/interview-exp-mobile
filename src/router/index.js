630d696d99254dd6da2b525a000da9ff8e8f20cc
u7663394 <u7663394@anu.edu.au>
2026-03-15 13:58:55 +1100

    Configure second-level route

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,11 @@
+import Article from "@/views/Article.vue";
+import Collect from "@/views/Collect.vue";
 import Detail from "@/views/Detail.vue";
 import Layout from "@/views/Layout.vue";
+import Like from "@/views/Like.vue";
 import Login from "@/views/Login.vue";
 import Register from "@/views/Register.vue";
+import User from "@/views/User.vue";
 import Vue from "vue";
 import VueRouter from "vue-router";
 
@@ -11,7 +15,17 @@ const router = new VueRouter({
   routes: [
     { path: "/login", component: Login },
     { path: "/register", component: Register },
-    { path: "/layout", component: Layout },
+    {
+      path: "/",
+      component: Layout,
+      redirect: "/article",
+      children: [
+        { path: "/article", component: Article },
+        { path: "/like", component: Like },
+        { path: "/collect", component: Collect },
+        { path: "/user", component: User },
+      ],
+    },
     { path: "/article/:id", component: Detail },
   ],
 });
---
+++ src/views/User.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="user-page">我的</div>
+</template>
+
+<script>
+export default {
+  name: "UserPage",
+  data() {
+    return {};
+  },
+  async created() {},
+  methods: {},
+};
+</script>
+
+<style lang="less" scoped></style>
