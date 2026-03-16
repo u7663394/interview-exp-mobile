ac86355716819f4f9335e9a969fd24ebd7a20628
u7663394 <u7663394@anu.edu.au>
2026-03-16 22:57:47 +1100

    Add global guard for unauthenticated access

--- src/router/index.js
+++ src/router/index.js
@@ -1,3 +1,4 @@
+import { getToken } from "@/utils/storage";
 import Article from "@/views/Article.vue";
 import Collect from "@/views/Collect.vue";
 import Detail from "@/views/Detail.vue";
@@ -11,6 +12,7 @@ import VueRouter from "vue-router";
 
 Vue.use(VueRouter);
 
+// 1. 配置路由
 const router = new VueRouter({
   routes: [
     { path: "/login", component: Login },
@@ -28,6 +30,22 @@ const router = new VueRouter({
     },
     { path: "/article/:id", component: Detail },
   ],
+});
+
+// 2. 全局前置守卫: 拦截处理
+const whiteList = ["/login", "/register"];
+router.beforeEach((to, from, next) => {
+  const token = getToken();
+  // 2.1. 有 token 放行
+  if (token) {
+    return next();
+  }
+  // 2.2. 如果去 whiteList, 放行
+  if (whiteList.includes(to.path)) {
+    return next();
+  }
+  // 2.3. 否则拦截
+  return next("/login");
 });
 
 export default router;
