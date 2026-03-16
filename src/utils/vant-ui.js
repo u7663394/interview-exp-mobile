0e75f69d6fa164690e4484d579cb6f4bbf92a322
u7663394 <u7663394@anu.edu.au>
2026-03-16 23:19:27 +1100

    Implement basic layout for Article.vue

--- src/utils/vant-ui.js
+++ src/utils/vant-ui.js
@@ -1,6 +1,15 @@
 // 按需导入 Vant
 import Vue from "vue";
-import { TabbarItem, Tabbar, Form, Field, Button, NavBar, Toast } from "vant";
+import {
+  TabbarItem,
+  Tabbar,
+  Form,
+  Field,
+  Button,
+  NavBar,
+  Toast,
+  Cell,
+} from "vant";
 Vue.use(TabbarItem);
 Vue.use(Tabbar);
 Vue.use(Field);
@@ -8,3 +17,4 @@ Vue.use(Form);
 Vue.use(Button);
 Vue.use(NavBar);
 Vue.use(Toast);
+Vue.use(Cell);
