183e966f5543d5630894d4c8236e5f4911ff783e
u7663394 <u7663394@anu.edu.au>
2026-03-17 23:32:20 +1100

    Implement Like and Collect components with feature

--- src/views/Collect.vue
+++ src/views/Collect.vue
@@ -1,18 +1,48 @@
 <template>
   <div class="collect-page">
-    <ArticleItem></ArticleItem>
-    <ArticleItem></ArticleItem>
+    <van-nav-bar fixed title="我的收藏" />
+    <van-list
+      v-model="loading"
+      :finished="finished"
+      finished-text="没有更多了"
+      @load="onLoad"
+    >
+      <ArticleItem v-for="item in list" :key="item.id" :item="item" />
+    </van-list>
   </div>
 </template>
 
 <script>
+import { getArticlesCollect } from "@/api/article";
 export default {
-  name: "CollectPage",
+  name: "collect-page",
   data() {
-    return {};
+    return {
+      list: [],
+      loading: false,
+      finished: false,
+      page: 1,
+    };
   },
-  methods: {},
+  methods: {
+    async onLoad() {
+      // 1. 更新数据
+      const { data } = await getArticlesCollect({ page: this.page });
+      this.list.push(...data.rows);
+      this.loading = false;
+      this.page++;
+      // 2. 结束文本
+      if (this.page > data.pageTotal) {
+        this.finished = true;
+      }
+    },
+  },
 };
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.collect-page {
+  margin-bottom: 50px;
+  margin-top: 44px;
+}
+</style>
--- src/views/Like.vue
+++ src/views/Like.vue
@@ -1,19 +1,48 @@
 <template>
   <div class="like-page">
-    <ArticleItem></ArticleItem>
-    <ArticleItem></ArticleItem>
-    <ArticleItem></ArticleItem>
+    <van-nav-bar fixed title="我的点赞" />
+    <van-list
+      v-model="loading"
+      :finished="finished"
+      finished-text="没有更多了"
+      @load="onLoad"
+    >
+      <ArticleItem v-for="item in list" :key="item.id" :item="item" />
+    </van-list>
   </div>
 </template>
 
 <script>
+import { getArticlesLike } from "@/api/article";
 export default {
-  name: "LikePage",
+  name: "like-page",
   data() {
-    return {};
+    return {
+      list: [],
+      loading: false,
+      finished: false,
+      page: 1,
+    };
   },
-  methods: {},
+  methods: {
+    async onLoad() {
+      // 1. 更新数据
+      const { data } = await getArticlesLike({ page: this.page });
+      this.list.push(...data.rows);
+      this.loading = false;
+      this.page++;
+      // 2. 结束文本
+      if (this.page > data.pageTotal) {
+        this.finished = true;
+      }
+    },
+  },
 };
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.like-page {
+  margin-bottom: 50px;
+  margin-top: 44px;
+}
+</style>
