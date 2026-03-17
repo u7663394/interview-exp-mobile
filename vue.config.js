aa04930366f7763ae5bd857972a2c6db0032ec7e
u7663394 <u7663394@anu.edu.au>
2026-03-18 09:31:25 +1100

    Configure publicPath to optimize build output

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  // 打包后是相对路径
+  publicPath: "./",
   css: {
     loaderOptions: {
       less: {
